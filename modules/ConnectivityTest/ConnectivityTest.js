b50476ab93ad80fa2121dc3aadaaf2e148dc1e9a
Wizard <66345978+Wizard89@users.noreply.github.com>
2023-02-20 13:54:57 +0800

    更新 ConnectivityTest.js

---
+++ modules/ConnectivityTest/ConnectivityTest.js
@@ -0,0 +1,29 @@
+//由本群重庆佬提供，key和小白脸大佬修改完善。
+let $ = {
+Baidu:'https://www.baidu.com',
+Github:'https://www.github.com',
+Google:'https://www.google.com/generate_204',
+Youtube:'https://www.youtube.com/'
+}
+
+!(async () => {
+await Promise.all([http('Baidu'),http('Github'),http('Google'),http('Youtube')]).then((x)=>{
+	$done({
+    title: 'Network Connectivity Test',
+    content: x.join('\n'),
+    icon: 'timer',
+    'icon-color': '#FF5A9AF9',
+  })
+})
+})();
+
+function http(req) {
+    return new Promise((r) => {
+			let time = Date.now();
+        $httpClient.post($[req], (err, resp, data) => {
+            r(req +
+						'\xa0\xa0\xa0\t: ' +
+						(Date.now() - time)+' ms');
+        });
+    });
+}
